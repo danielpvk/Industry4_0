5970470aaeff6b08edc2238a793d0d75eb5f5b31
danielpv <danielpvk@gmail.com>
2018-11-22 18:30:17 -0600

    version to merge without gauge

--- public/js/process.js
+++ public/js/process.js
@@ -1,9 +1,5 @@
 // Get references to page elements
-$.getScript("https://www.gstatic.com/charts/loader.js", function(){
 
-    alert("Script loaded but not necessarily executed.");
- 
- });
 
 var $div1=$("#div1");
 var $d1=$("#d1");
@@ -26,6 +22,7 @@ var $idD4=$("#serial4");
 var $type4=$("#type4");
 var $idD5=$("#serial5");
 var $type5=$("#type5");
+var $graph1=$("#graph1");
 // The API object contains methods for retrieve the lectures of the devices
 var API_Device = {
   getDevices: function() {
@@ -57,6 +54,84 @@ var API_D = {
       });
     },
   };
+
+ /*  function drawChart(parametersNames,actualValues,minValues,maxValues) {
+        var graphic=[];
+        var graphics=[];
+        for (var i=0;i<parametersNames.length;i++){
+            graphic=[];
+            graphic.push(parametersNames[i],actualValues[i]);
+            graphics.push(graphic);
+            console.log(graphic);
+            console.log (graphics);
+        }
+        
+        var data = google.visualization.arrayToDataTable([
+        ['Label', 'Value'],
+        ['Memory', 80],
+        ['CPU', 55],
+        ['Network', 68]
+        ]);
+
+        var options = {
+        width: 400, height: 120,
+        redFrom: 90, redTo: 100,
+        yellowFrom:75, yellowTo: 90,
+        minorTicks: 5
+        };
+
+        var chart = new google.visualization.Gauge(document.getElementById('graph1'));
+
+        chart.draw(data, options);
+
+        setInterval(function() {
+        data.setValue(0, 1, 40 + Math.round(60 * Math.random()));
+        chart.draw(data, options);
+        }, 13000);
+        setInterval(function() {
+        data.setValue(1, 1, 40 + Math.round(60 * Math.random()));
+        chart.draw(data, options);
+        }, 5000);
+        setInterval(function() {
+        data.setValue(2, 1, 60 + Math.round(20 * Math.random()));
+        chart.draw(data, options);
+        }, 26000);
+  }
+
+ */
+function drawChart() {
+
+    var data = google.visualization.arrayToDataTable([
+      ['Label', 'Value'],
+      ['Memory', 80],
+      ['CPU', 55],
+      ['Network', 68]
+    ]);
+
+    var options = {
+      width: 400, height: 120,
+      redFrom: 90, redTo: 100,
+      yellowFrom:75, yellowTo: 90,
+      minorTicks: 5
+    };
+
+    var chart = new google.visualization.Gauge(document.getElementById('chart_div'));
+
+    chart.draw(data, options);
+
+    setInterval(function() {
+      data.setValue(0, 1, 40 + Math.round(60 * Math.random()));
+      chart.draw(data, options);
+    }, 13000);
+    setInterval(function() {
+      data.setValue(1, 1, 40 + Math.round(60 * Math.random()));
+      chart.draw(data, options);
+    }, 5000);
+    setInterval(function() {
+      data.setValue(2, 1, 60 + Math.round(20 * Math.random()));
+      chart.draw(data, options);
+    }, 26000);
+  }
 
 // Script to print the lectures of the devices
 var getDeviceLastLecture = function(divtype,serie,type) {
@@ -117,11 +192,12 @@ var getDeviceLastLecture = function(divtype,serie,type) {
                     divtype.append(aux);
                     console.log("aux: ",aux);
                 }
+                
             });
     });
 };
 
-//Add the device types to each of the device_types divs
+//check if the parameter exist, if the parameter=0, cleans the screen in the parameter div
 var ifParameter=function(id,divData,divText,typeD){
     if (id!=0){
         getDeviceLastLecture(divData,id,typeD);
@@ -131,33 +207,20 @@ var ifParameter=function(id,divData,divText,typeD){
         divText.html("");
     }
 }
-
+//Call the function to fill the data of each parameter
 var refreshDeviceLectures=function(){
-  /*   if ($idD1.text()!=0){
-        getDeviceLastLecture($div1,$idD1.text(),$type1.text());
-    }
-    if ($idD2.text()!=0){
-        getDeviceLastLecture($div2,$idD2.text(),$type2.text());
-    }
-    else
-    {$d2.empty();}
-    if ($idD3.text()!=0){
-        console.log("idD3=",$idD3);
-        getDeviceLastLecture($div2,$idD2.text(),$type2.text());
-    }
-    else
-    {
-        console.log("idD3=",$idD3);
-        $d3.html("");
-    }
-    getDeviceLastLecture($div2,$idD2.text(),$type2.text());
-    getDeviceLastLecture($div2,$idD2.text(),$type2.text()); */
     ifParameter($idD1.text(),$div1,$d1,$type1.text());
+    
     ifParameter($idD2.text(),$div2,$d2,$type2.text());
     ifParameter($idD3.text(),$div3,$d3,$type3.text());
     ifParameter($idD4.text(),$div4,$d4,$type4.text());
     ifParameter($idD5.text(),$div5,$d5,$type5.text());
 };
-
+//google.charts.load('current', {'packages':['gauge']});
+//google.charts.setOnLoadCallback(drawChart());
 refreshDeviceLectures();
+
+
+
+
 
