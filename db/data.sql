b6d80e48677c4d2de7c46542f0e591a6d3314755
Danielpvk <danielpvk@gmail.com>
2018-11-24 09:57:29 -0600

    almost ready graphic interface

--- db/data.sql
+++ db/data.sql
@@ -18,6 +18,15 @@ VALUES ("Motor 5hp",3,"Current consumption",12,22,"Temperature(C)",5,80,"vibrati
 INSERT INTO devicetypes (DeviceTypeDescription,Parameters_quantity,Parameter1_name,Parameter1_min_val,Parameter1_max_val,Parameter2_name,Parameter2_min_val,Parameter2_max_val)
 VALUES ("Temperature/humidity Sensor",1,"Temperature",180,220,"Humidity (%)",40,70);
 
+INSERT INTO devicetypes (DeviceTypeDescription,Parameters_quantity,Parameter1_name,Parameter1_min_val,Parameter1_max_val,Parameter2_name,Parameter2_min_val,Parameter2_max_val)
+VALUES ("Temperature/humidity Sensor -Low Temp",2,"Temperature",0,45,"Humidity (%)",40,80);
+
+INSERT INTO devicetypes (DeviceTypeDescription,Parameters_quantity,Parameter1_name,Parameter1_min_val,Parameter1_max_val)
+VALUES ("RFID Counter",2,"Pieces/hr",0,500);
+
+INSERT INTO devicetypes (DeviceTypeDescription,Parameters_quantity,Parameter1_name,Parameter1_min_val,Parameter1_max_val,Parameter2_name,Parameter2_min_val,Parameter2_max_val)
+VALUES ("Pressure Sensor",2,"Pressure (psi)",50,150,"Flow (cfm)",10,20);
+
 INSERT INTO devices (LectureP1,LectureP2,NumSerie) 
 VALUES (15,12,1234);
 
